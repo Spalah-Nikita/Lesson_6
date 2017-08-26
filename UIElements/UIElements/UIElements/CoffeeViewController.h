46b3b54d1f988eeb5ce132dbcfb82184fbdc9b35
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-08-26 15:13:38 +0300

    Добавил экран для домашнего задания

---
+++ UIElements/UIElements/UIElements/CoffeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoffeeViewController.h
+//  UIElements
+//
+//  Created by Nikita Vintonovich on 8/26/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoffeeViewController : UIViewController
+
+@end
---
+++ UIElements/UIElements/UIElements/CoffeeViewController.m
@@ -0,0 +1,27 @@
+//
+//  CoffeeViewController.m
+//  UIElements
+//
+//  Created by Nikita Vintonovich on 8/26/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import "CoffeeViewController.h"
+
+@interface CoffeeViewController ()
+
+@end
+
+@implementation CoffeeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)backAction:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
